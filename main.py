767dbf1b70801bd5b708fa0d6b36ff186d2e6e5f
kaapomoi <56117900+kaapomoi@users.noreply.github.com>
2020-08-10 22:09:46 +0300

    Added support for 3 million different things, yep

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import sys
 import os
 import random
+import math
 from spotipy.oauth2 import SpotifyClientCredentials
 
 import spotipy
@@ -9,18 +10,30 @@ import spotipy.util as util
 
 SCOPE = 'user-library-read playlist-modify-private playlist-modify-public'
 
+# remember to set this uri in spotify devportal aswell.
 REDIRECT_URI='http://localhost:8888/callback/'
 
 parser = argparse.ArgumentParser(description='create disarray in a playlist')
+parser.add_argument('client_secret', type=str)
+parser.add_argument('client_id', type=str)
 parser.add_argument('username', type=str)
 parser.add_argument('playlist_name', type=str)
 
 args = parser.parse_args()
 
+client_secret = args.client_secret
+CLIENT_ID=args.client_id
 username = args.username
 playlist_name = args.playlist_name
 
-sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+token = util.prompt_for_user_token(username, SCOPE,CLIENT_ID,client_secret,REDIRECT_URI)
+
+if token:
+    sp = spotipy.Spotify(auth=token)
+else:
+    sys.exit('Bad token')
+
+#sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
 playlist_id_to_modify = ''
 
@@ -31,22 +44,50 @@ for i in lists['items']:
 
 # load the first 100 songs
 tracks = []
-result = sp.playlist_tracks(playlist_id_to_modify, additional_types=['track'])
+result = sp.playlist_tracks(playlist_id_to_modify, fields='items.track.id, next', additional_types=['track'])
 tracks.extend(result['items'])
-
 
 # if playlist is larger than 100 songs, continue loading it until end
 while result['next']:
     result = sp.next(result)
     tracks.extend(result['items'])
 
-# remove all local songs
-i = 0  # just for counting how many tracks are local
-for item in tracks:
-    if item['is_local']:
-        tracks.remove(item)
-        i += 1
+#print(tracks)
 
+t = []
+for track in tracks:
+    s = str(track)
+    t.append(s[18:40])
 
-# print result
-print("Playlist length: " + str(len(tracks)) + "\nExcluding: " + str(i))
+#print(t)
+
+# shuffle the tracks, randomly
+random.shuffle(t)
+
+nr_tracks = len(t)
+hunds = math.floor(nr_tracks / 100)
+overflow = nr_tracks - (hunds * 100)
+hunds_c = hunds
+
+print("hunds " + str(hunds))
+print("overflow " + str(overflow))
+
+i = 0
+
+# remove all tracks for playlist
+while hunds_c >= 1:
+    sp.user_playlist_remove_all_occurrences_of_tracks(username, playlist_id_to_modify, t[i:i+99])
+    hunds_c -= 1
+    i += 100
+
+sp.user_playlist_remove_all_occurrences_of_tracks(username, playlist_id_to_modify, t[hunds*100:hunds*100+overflow])
+
+i = 0
+hunds_c = hunds
+# add the disarrayed tracks to the playlist
+while hunds_c >= 1:
+    sp.user_playlist_add_tracks(username, playlist_id_to_modify, t[i:i+99])
+    hunds_c -= 1
+    i += 100
+
+sp.user_playlist_add_tracks(username, playlist_id_to_modify, t[hunds*100:hunds*100+overflow])
